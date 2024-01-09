16496f9d63019a9bc117d81fb66b330b4e429987
Antoine Verin <averin@student.42.fr>
2024-01-09 13:05:49 +0100

    remove prototype and add milliseconds fonction

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 10:57:39 by averin            #+#    #+#             */
-/*   Updated: 2024/01/06 14:42:29 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/09 12:56:19 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include <pthread.h>
 # include <unistd.h>
 # include <stdio.h>
+# include <sys/time.h>
 
 # define ERROR_USAGE "Usage: %s number_of_philosophers time_to_die time_to_eat \
 time_to_sleep [number_of_times_each_philosopher_must_eat]\n"
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,51 +6,24 @@
 /*   By: averin <averin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 10:58:01 by averin            #+#    #+#             */
-/*   Updated: 2024/01/06 16:21:50 by averin           ###   ########.fr       */
+/*   Updated: 2024/01/09 13:02:55 by averin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-#define TIMES_TO_COUNT 21000
-
-void	*test(void *arg)
+long	get_miliseconds()
 {
-	int	i;
-	int	*j = (int *) arg;
+	struct timeval time;
 
-	(void)arg;
-	i = -1;
-	while (++i < TIMES_TO_COUNT)
-	{
-		(*j)++;
-		// printf("hello for the %dth time\n", *j);
-		// usleep(1000000);
-	}
-	return (0);
+	if (gettimeofday(&time, NULL) == -1)
+		return (printf("/!\\ time error\n"), -1);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
 int	main(int argc, char const *argv[])
 {
-	pthread_t		thread;
-	pthread_t		threadt;
-	pthread_mutex_t	mutex;
-	int				i;
-	int				j;
-
-	(void)argc;
-	(void)argv;
-	// if (argc < 5 || argc > 6)
-	// 	return (printf(ERROR_USAGE, argv[0]), 1);
-	i = -1;
-	j = 0;
-	pthread_mutex_init(&mutex, NULL);
-	pthread_create(&thread, NULL, &test, &j);
-	pthread_create(&threadt, NULL, &test, &j);
-	// printf("Hello from parent\n");
-	pthread_join(thread, NULL);
-	pthread_join(threadt, NULL);
-	pthread_mutex_destroy(&mutex);
-	printf("parent ended %d\n", j);
+	if (argc < 5 || argc > 6)
+		return (printf(ERROR_USAGE, argv[0]), 1);
 	return (0);
 }
